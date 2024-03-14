d7b4bc3f82e696b3ec369e96f0560430e3126d3f
Paolo Cesar Guillen Lupo <paologuillenlupo@gmail.com>
2024-03-13 19:45:25 -0500

    cambios menores

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(
-    sequence(preprocessMeltUI())
-  ),
+  preprocess: vitePreprocess()
 };
