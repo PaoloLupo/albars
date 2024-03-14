fc57a9097b42a27e74ac58d9e036afb25f3fe140
Paolo Cesar Guillen Lupo <paologuillenlupo@gmail.com>
2024-03-13 19:39:16 -0500

    eliminando codigo innecesario y haciendo publico los modulos a usar para la coleccion del api

--- src-tauri/crates/alba-api/src/lib.rs
+++ src-tauri/crates/alba-api/src/lib.rs
@@ -1,19 +1,6 @@
 mod bindings;
 mod helpers;
-mod connect;
-mod instances;
+pub mod connect;
+pub mod instances;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
