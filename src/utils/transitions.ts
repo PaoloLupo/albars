1f3ac9cb040a81777d284b33628b183b4f1dc235
Paolo Cesar Guillen Lupo <paologuillenlupo@gmail.com>
2024-03-13 19:43:27 -0500

    adicionando una transicion para los menus

---
+++ src/utils/transitions.ts
@@ -0,0 +1,47 @@
+import type {TransitionConfig} from "svelte/transition";
+import {cubicOut} from "svelte/easing";
+
+type FlyAndScaleParams = {
+    y?: number;
+    start?: number;
+    duration?: number;
+};
+
+const defaultFlyAndScaleParams= {y:-8, start: 0.95, duration: 200};
+
+export const flyAndScale = (node: Element, params?: FlyAndScaleParams): TransitionConfig => {
+    const style = getComputedStyle(node);
+    const transform = style.transform === "none"? "": style.transform;
+
+    const withDefaults = {...defaultFlyAndScaleParams, ...params};
+
+    const scaleConversion = (valueA: number, scaleA: [number, number], scaleB: [number, number]) => {
+        const [minA, maxA] = scaleA;
+        const [minB, maxB] = scaleB;
+
+        const percentage = (valueA - minA) / (maxA - minA);
+        return percentage * (maxB - minB) + minB;
+    };
+
+	const styleToString = (style: Record<string, number | string |undefined>): string => {
+		return Object.keys(style).reduce((str, key) => {
+			if (style[key] === undefined) return str;
+			return str + `${key}:${style[key]};`;
+		}, "");
+	}
+
+	return {
+		duration: withDefaults.duration ?? 200,
+		delay: 0,
+		css: (t) => {
+			const y = scaleConversion(t, [0, 1], [withDefaults.y, 0]);
+			const scale = scaleConversion(t, [0, 1], [withDefaults.start, 1]);
+
+			return styleToString({
+				transform: `${transform} translate3d(0, ${y}px, 0) scale(${scale})`,
+				opacity: t,
+			});
+		},
+		easing: cubicOut,
+	};
+}
