47173e64ed2852d56aaef38a4a1db149a70c6f8d
Paolo Cesar Guillen Lupo <paologuillenlupo@gmail.com>
2024-03-14 19:19:31 -0500

    prueba con svelte 5 y tailwind next

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import "./styles.css";
 import App from "./App.svelte";
+import {mount} from "svelte";
 
+//config para svelte 4
 // @ts-ignore
-const app = new App({
-  target: document.getElementById("app"),
-});
+// const app = new App({
+//   target: document.getElementById("app"),
+// });
+
+// nuevo svelte 5
+const app = mount(App,{target: document.getElementById("app")});
 
 export default app;
